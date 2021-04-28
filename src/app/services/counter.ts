06af3ed3e7ee709f0e523a0a32fe426a10ac588a
Your Name <you@example.com>
2021-04-28 12:12:30 +0200

    Creazione del sevizio

---
+++ src/app/services/counter.ts
@@ -0,0 +1,38 @@
+export class Counter {
+
+    private max:number;
+    private min:number;
+    private initial:number;
+
+    private value:number;
+    constructor(initial = 0,min = 0,max = 0){
+
+        this.value = initial
+        this.max = max === null ? null : Number(max)
+        this.min = min === null ? null : Number(min)
+        this.initial = initial;
+
+        if(this.min !== null && this.initial < this.min) {throw new Error('initial è minore di min')}
+        if(this.max !== null && this.initial > this.max) {throw new Error('initial è maggiore di max')}
+    }
+
+    
+    increment(){
+
+        if(this.max === null) {
+            this.value++
+        }else if(this.value < this.max ){
+            this.value++ 
+        }
+        
+    }
+
+    decrement(){
+       
+        if(this.min === null) {
+            this.value--
+        }else if(this.value > this.min ){
+            this.value-- 
+        }
+    }
+}
